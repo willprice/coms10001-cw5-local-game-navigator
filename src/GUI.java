0d56460e260256a67cdc88082531b1ae1ecf9de4
Jamie Maddocks <jamie@chicken-studios.com>
2014-03-03 08:43:16 +0000

    Began swing gui work.

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,53 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Main visualising class
  *
  */
 public class GUI extends GameVisualiser {
-		public void run()
-		{
-			//entry point for implementing your graphics window
+	private JFrame frame;
+		
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public void run() {
+		frame = new JFrame();
+		frame.setPreferredSize(new Dimension(1000, 600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		//panel.setPreferredSize(new Dimension(600, 800));
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		Box horizontalBox = Box.createHorizontalBox();
+		panel.add(horizontalBox, Component.CENTER_ALIGNMENT);
+		
+		Box verticalBox = Box.createVerticalBox();
+		horizontalBox.add(verticalBox, JComponent.CENTER_ALIGNMENT);
+		
+		JLabel lblNewLabel = new JLabel("");
+		URL url = getClass().getResource("resource/map.jpg");
+		try {
+			Image mapImage = ImageIO.read(getClass().getResource("/map.jpg"));
+			mapImage = mapImage.getScaledInstance(mapImage.getWidth(null) / 2, mapImage.getHeight(null) / 2, Image.SCALE_DEFAULT);
+			
+			lblNewLabel.setIcon(new ImageIcon(mapImage));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		verticalBox.add(lblNewLabel);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
--- src/Game.java
+++ src/Game.java
@@ -14,5 +14,6 @@ public class Game {
 		gui.registerMapVisualisable(state);
 		
 		//initialise then start your GUI
+		gui.run();
 	}
 }
